98eac7a3cfcefd8b044ea09d67d3e0ef6d6450a5
Robin Naghshbandi <robinnaghshbandi12@gmail.com>
2023-06-24 17:55:16 +0100

    update

--- edustar-front/src/app/page.js
+++ edustar-front/src/app/page.js
@@ -22,23 +22,6 @@ useEffect(() => {
           Get started by editing&nbsp;
           <code className={styles.code}>src/app/page.js</code>
         </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
       </div>
 
       {/* <div className={styles.center}> */}
