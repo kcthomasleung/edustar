71f5fa8c220dd01d90a8f2061f929bd4870502ae
nmaskens <66023811+nmaskens@users.noreply.github.com>
2023-06-24 23:49:56 +0100

    FrontendBoilerplate-2

---
+++ edustar-frontend/.next/temp/components/NFTBox.js
@@ -0,0 +1,137 @@
+import { useEffect, useState } from "react"
+import { useWeb3Contract, useMoralis } from "react-moralis"
+import NftMarketplaceAbi from "./constants/NftMarketplace.json"
+import nftAbi from "./constants/BasicNft.json"
+import Image from "next/image"
+import { Card, useNotification } from "web3uikit"
+import { ethers } from "ethers"
+import UpdateListingModal from "./UpdateListingModal"
+
+const truncateStr = (fullStr, strLen) => {
+    if (fullStr.length <= strLen) return fullStr
+
+    const separator = ""
+    const separatorLength = separator.length
+    const charsToShow = strLen - separatorLength
+    const frontChars = Math.ceil(charsToShow / 2)
+    const backChars = Math.floor(charsToShow / 2)
+    return (
+        fullStr.subString(0, frontChars) + separator + fullStr.subString(fullStr.length - backChars)
+    )
+}
+
+export default function NFTBox({ price, nftAddress, tokenId, marketplaceAddress, seller }) {
+    const { isWeb3Enabled, account } = useMoralis()
+    const [imageURI, setImageURI] = useState("")
+    const [tokenName, setTokenName] = useState("")
+    const [tokenDescription, setTokenDescription] = useState("")
+    const [showModal, setShowModal] = useState(false)
+    const hideModal = () => setShowModal(false)
+    const dispatch = useNotification()
+
+    const { runContractFunction: getTokenURI } = useWeb3Contract({
+        abi: nftAbi,
+        contractAddress: nftAddress,
+        functionName: "tokenURI",
+        params: {
+            tokenId: tokenId,
+        },
+    })
+
+    const { runContractFunction: buyItem } = useWeb3Contract({
+        abi: NftMarketplaceAbi,
+        contractAddress: marketplaceAddress,
+        functionName: "buyItem",
+        msgValue: price,
+        params: {
+            nftAddress: nftAddress,
+            tokenId: tokenId,
+        },
+    })
+
+    async function updateURI() {
+        const tokenURI = await getTokenURI()
+        console.log(`The TokenURI is${tokenURI}`)
+        if (tokenURI) {
+            const requestURL = tokenURI.replace("ipfs://", "https://ipfs.io/ipfs/")
+            const tokenURIResponse = await (await fetch(requestURL)).json()
+            const imageURI = tokenURIResponse.image
+            const imageURIURL = imageURI.replace("ipfs://", "https://ipfs.io/ipfs/")
+            setImageURI(imageURIURL)
+            setTokenName(tokenURIResponse.name)
+            setTokenDescription(tokenURIResponse.description)
+        }
+        // Get the tokenURI
+        // Using the image from the tokenURI, get the image
+    }
+
+    useEffect(() => {
+        if (isWeb3Enabled) {
+            updateURI()
+        }
+    }, [isWeb3Enabled])
+
+    const isOwnedByUser = seller === account || seller === undefined
+    const formattedSellerAddress = isOwnedByUser ? "you" : truncateStr(seller || "", 15)
+
+    const handleCardClick = () => {
+        isOwnedByUser
+            ? setShowModal(true)
+            : buyItem({
+                  onError: (error) => console.log(error),
+                  onSuccess: () => handleBuyItemSuccess(),
+              })
+    }
+
+    const handleBuyItemSuccess = () => {
+        dispatch({
+            type: "success",
+            message: "Item Bought!",
+            title: "Item Bought",
+            position: "topR",
+        })
+    }
+
+    return (
+        <div>
+            <div>
+                {imageURI ? (
+                    <div>
+                        <UpdateListingModal
+                            isVisible={showModal}
+                            tokenId={tokenId}
+                            marketplaceAddress={marketplaceAddress}
+                            nftAddress={nftAddress}
+                            onClose={HideModal}
+                        />
+                        <Card
+                            title={tokenName}
+                            description={tokenDescription}
+                            onClick={handleCardClick}
+                        >
+                            <div className="p-2">
+                                <div className="flex flex-col items-end gap-2">
+                                    <div>#{tokenId}</div>
+                                    <div className="italic text-sm">
+                                        Owned by {formattedSellerAddress}
+                                    </div>
+                                    <Image
+                                        loader={() => imageURI}
+                                        src={imageURI}
+                                        height="200"
+                                        width="200"
+                                    />
+                                    <div className="font-bold">
+                                        {ethers.utils.formatUnits(price, "ether")} ETH
+                                    </div>
+                                </div>
+                            </div>
+                        </Card>
+                    </div>
+                ) : (
+                    <div>Loading...</div>
+                )}
+            </div>
+        </div>
+    )
+}
---
+++ edustar-frontend/.next/temp/components/UpdateListingModal.js
@@ -0,0 +1,66 @@
+import { Modal, Input, useNotification } from "@web3uikit/core"
+import { useState } from "react"
+import { useWeb3Contract } from "react-moralis"
+import { ethers } from "ethers"
+import { handleClientScriptLoad } from "next/script"
+
+export default function UpdateListingModal({
+    nftAddress,
+    tokenId,
+    isVisible,
+    marketplaceAddress,
+    onClose,
+}) {
+    const dispatch = useNotification()
+
+    const [priceToUpdateListingWith, setPriceToUpdateListingWith] = useState(0)
+
+    const handleUpdateListingSuccess = async (tx) => {
+        await tx.wait(1)
+        dispatch({
+            type: "success",
+            message: "Listing updated",
+            title: "Listing updated - please refresh (and move blocks)",
+            position: "topR",
+        })
+        onClose && onClose()
+        setPriceToUpdateListingWith("0")
+    }
+
+    const { runContractFunction: updateListing } = useWeb3Contract({
+        abi: nftMarketplaceAbi,
+        contractAddress: marketplaceAddress,
+        functionName: "updateListing",
+        params: {
+            nftAddress: nftAddress,
+            tokenId: tokenId,
+            newPrice: ethers.utils.parseEther(priceToUpdateListingWith || "0"),
+        },
+    })
+
+    return (
+        <Modal
+            isVisible={isVisible}
+            onCancel={onClose}
+            onCloseButtonPressed={onClose}
+            onOk={() => {
+                updateListing({
+                    onError: (error) => {
+                        console.log(error)
+                    },
+                    onSuccess: handleUpdateListingSuccess,
+                })
+            }}
+        >
+            <Input
+                label="Update listing price in L1 Currency (ETH)"
+                name="New listing price"
+                type="number"
+                onChange={(event) => {
+                    setPriceToUpdateListingWith(event.target.value)
+                }}
+                onOk={() => {}}
+            />
+        </Modal>
+    )
+}
