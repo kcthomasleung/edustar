d5d42660e326be652895ee3960b109d5c800c0e9
Robin Naghshbandi <robinnaghshbandi12@gmail.com>
2023-06-25 04:42:07 +0100

    qa

--- AI_Review_ApI/main.py
+++ AI_Review_ApI/main.py
@@ -1,5 +1,6 @@
 from typing import Union
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from sentence_transformers import SentenceTransformer, util
 import torch
 import pandas as pd
@@ -12,10 +13,8 @@ sentiment_pipeline = pipeline("sentiment-analysis")
 df = pd.read_csv("coursea_data.csv")
 qa = pd.read_csv("qa.csv", header=None)
 
-corpus = df.iloc[:,1].to_list()
+corpus = df.iloc[:, 1].to_list()
 corpus_embeddings = embedder.encode(corpus, convert_to_tensor=True)
-
-
 
 model = SentenceTransformer('multi-qa-MiniLM-L6-cos-v1')
 
@@ -37,24 +36,27 @@ def search(text):
         for score, idx in zip(top_results[0], top_results[1]):
             idxs.append(int(idx))
 
-
-    return df.iloc[idxs,:].to_json( orient='records', lines=True)
-    
+    return df.iloc[idxs, :].to_json(orient='records', lines=True)
 
 
-def similarity_check(k,text):
-
+def similarity_check(k, text):
     sentences1 = text
-    sentences2 = [qa.iloc[k,0]]
+    sentences2 = [qa.iloc[k, 0]]
     embeddings1 = model.encode(sentences1, convert_to_tensor=True)
     embeddings2 = model.encode(sentences2, convert_to_tensor=True)
     cosine_scores = util.cos_sim(embeddings1, embeddings2)
     return float(cosine_scores[0][0])
 
 
-
 app = FastAPI()
 
+# Add CORS middleware to allow cross-origin requests
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Update this list with your specific domain(s)
+    allow_methods=["GET"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def read_root():
@@ -66,12 +68,11 @@ def read_item(text: Union[str, None] = None):
     return review(text)
 
 
-
 @app.get("/search/{text}")
 def read_item(text: Union[str, None] = None):
     return search(text)
 
 
 @app.get("/similarity/{id}/{text}")
-def read_item(id:int ,text: Union[str, None] = None):
+def read_item(id: int, text: Union[str, None] = None):
     return similarity_check(id, text)
